5c72b5fce72967c29ea9e46e5d7c57e09e81a667
Armando Gonzalez <iam@armando.mx>
2016-05-16 14:45:20 -0500

    Using todos template for app

---
+++ imports/ui/pages/AuthPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import MobileMenu from '../components/MobileMenu.jsx';
+
+// a common layout wrapper for auth pages
+const AuthPage = ({ content, link }) => (
+  <div className="page auth">
+    <div className="content-scrollable">
+      {content}
+      {link}
+    </div>
+  </div>
+);
+
+AuthPage.propTypes = {
+  content: React.PropTypes.element,
+  link: React.PropTypes.element,
+};
+
+export default AuthPage;
---
+++ imports/ui/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MobileMenu from '../components/MobileMenu.jsx';
+import Message from '../components/Message.jsx';
+
+const NotFoundPage = () => (
+  <div className="page not-found">
+    <div className="content-scrollable">
+      <Message title="Page not found"/>
+    </div>
+  </div>
+);
+
+export default NotFoundPage;
